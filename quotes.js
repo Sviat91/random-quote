3d8fee3263ce5c6ff1a8e0eb64e0706e5c8aa80d
Sviat91 <svyatoslav.upirov@gmail.com>
2026-01-15 16:43:52 +0100

    refactor: separate quotes into a new module and update script type in HTML

---
+++ quotes.js
@@ -0,0 +1,229 @@
+const quotes = [
+  // Steve Jobs (Originals)
+  {
+    quote: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  // Business & Tech
+  {
+    quote:
+      "Success is a lousy teacher. It seduces smart people into thinking they can't lose",
+    author: 'Bill Gates',
+  },
+  {
+    quote:
+      'When something is important enough, you do it even if the odds are not in your favor',
+    author: 'Elon Musk',
+  },
+  {
+    quote:
+      'If you double the number of experiments you do per year you are going to double your inventiveness',
+    author: 'Jeff Bezos',
+  },
+  {
+    quote: 'Failure is the condiment that gives success its flavor',
+    author: 'Truman Capote',
+  },
+  {
+    quote:
+      'Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it',
+    author: 'Johann Wolfgang von Goethe',
+  },
+  // Wisdom & Philosophy
+  {
+    quote: 'The only true wisdom is in knowing you know nothing',
+    author: 'Socrates',
+  },
+  {
+    quote:
+      'Life is not a problem to be solved, but a reality to be experienced',
+    author: 'Søren Kierkegaard',
+  },
+  {
+    quote: 'He who has a why to live can bear almost any how',
+    author: 'Friedrich Nietzsche',
+  },
+  {
+    quote: 'Waste no more time arguing about what a good man should be. Be one',
+    author: 'Marcus Aurelius',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop',
+    author: 'Confucius',
+  },
+  // Science
+  {
+    quote: 'Imagination is more important than knowledge',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'However difficult life may seem, there is always something you can do and succeed at',
+    author: 'Stephen Hawking',
+  },
+  {
+    quote: 'Nothing in life is to be feared, it is only to be understood',
+    author: 'Marie Curie',
+  },
+  // Historical & Political
+  {
+    quote:
+      'Success is not final, failure is not fatal: it is the courage to continue that counts',
+    author: 'Winston Churchill',
+  },
+  {
+    quote:
+      'In the end, we will remember not the words of our enemies, but the silence of our friends',
+    author: 'Martin Luther King Jr.',
+  },
+  {
+    quote: "It always seems impossible until it's done",
+    author: 'Nelson Mandela',
+  },
+  {
+    quote: 'Be the change that you wish to see in the world',
+    author: 'Mahatma Gandhi',
+  },
+  {
+    quote: 'Do one thing every day that scares you',
+    author: 'Eleanor Roosevelt',
+  },
+  // Arts & Literature
+  {
+    quote: 'Be yourself; everyone else is already taken',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote:
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe",
+    author: 'Albert Einstein',
+  },
+  {
+    quote: 'So many books, so little time',
+    author: 'Frank Zappa',
+  },
+  {
+    quote: 'A room without books is like a body without a soul',
+    author: 'Marcus Tullius Cicero',
+  },
+  {
+    quote: 'You only live once, but if you do it right, once is enough',
+    author: 'Mae West',
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing',
+    author: 'Walt Disney',
+  },
+  {
+    quote: "If you tell the truth, you don't have to remember anything",
+    author: 'Mark Twain',
+  },
+  {
+    quote: 'A friend is someone who knows all about you and still loves you',
+    author: 'Elbert Hubbard',
+  },
+  {
+    quote:
+      'To live is the rarest thing in the world. Most people exist, that is all',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote: 'Always forgive your enemies; nothing annoys them so much',
+    author: 'Oscar Wilde',
+  },
+  {
+    quote:
+      'Live as if you were to die tomorrow. Learn as if you were to live forever',
+    author: 'Mahatma Gandhi',
+  },
+  {
+    quote: 'We accept the love we think we deserve',
+    author: 'Stephen Chbosky',
+  },
+  {
+    quote: 'Without music, life would be a mistake',
+    author: 'Friedrich Nietzsche',
+  },
+  {
+    quote:
+      "I am so clever that sometimes I don't understand a single word of what I am saying",
+    author: 'Oscar Wilde',
+  },
+  {
+    quote:
+      'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    quote:
+      'Insanity is doing the same thing, over and over again, but expecting different results',
+    author: 'Narcotics Anonymous',
+  },
+  {
+    quote:
+      'It is better to be hated for what you are than to be loved for what you are not',
+    author: 'Andre Gide',
+  },
+  {
+    quote:
+      'The person, be it gentleman or lady, who has not pleasure in a good novel, must be intolerably stupid',
+    author: 'Jane Austen',
+  },
+  {
+    quote:
+      'It is our choices, Harry, that show what we truly are, far more than our abilities',
+    author: 'J.K. Rowling',
+  },
+  {
+    quote: 'There is no greater agony than bearing an untold story inside you',
+    author: 'Maya Angelou',
+  },
+  {
+    quote: 'Everything you can imagine is real',
+    author: 'Pablo Picasso',
+  },
+  {
+    quote:
+      'You can never get a cup of tea large enough or a book long enough to suit me',
+    author: 'C.S. Lewis',
+  },
+  {
+    quote: "Life isn't about finding yourself. Life is about creating yourself",
+    author: 'George Bernard Shaw',
+  },
+  {
+    quote: 'Do what you can, with what you have, where you are',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    quote:
+      'Happiness is not something ready made. It comes from your own actions',
+    author: 'Dalai Lama',
+  },
+  {
+    quote: 'Whatever you are, be a good one',
+    author: 'Abraham Lincoln',
+  },
+  {
+    quote: 'Turn your wounds into wisdom',
+    author: 'Oprah Winfrey',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    quote: "Don't count the days, make the days count",
+    author: 'Muhammad Ali',
+  },
+];
+
+export default quotes;
