5392bc7e3eb89eff27f4a7aa88d31e63265af475
Sviat91 <svyatoslav.upirov@gmail.com>
2026-01-15 17:58:39 +0100

    feat: add favorite button and update quotes structure with isFavorite property

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
 const authorElement = document.getElementById('author');
+const togglefavoriteBtn = document.getElementById('favorite-btn');
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   quoteElement.textContent = randomQuote.quote;
   authorElement.textContent = randomQuote.author;
 }
+
+//function toggleFavotite()
 
 generateBtn.addEventListener('click', generateRandomQuote);
