7c612c90cd0bea21a5b7a901ae3084a93f807d4d
Sviat91 <svyatoslav.upirov@gmail.com>
2026-01-17 17:36:07 +0100

    feat: implement favorite functionality for quotes and update styles

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
 const authorElement = document.getElementById('author');
-const togglefavoriteBtn = document.getElementById('favorite-btn');
+const toggleFavoriteBtn = document.getElementById('favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
 
 let currentQuoteIndex;
 
@@ -12,8 +13,33 @@ function generateRandomQuote() {
   const randomQuote = quotes[currentQuoteIndex];
   quoteElement.textContent = randomQuote.quote;
   authorElement.textContent = randomQuote.author;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? 'Remove from favorites'
+    : 'Add to favorites';
 }
 
-//function toggleFavotite()
+function toggleFavotite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? 'Remove from favorites'
+    : 'Add to favorites';
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favorite-card');
+    favoriteCard.innerHTML = `
+      <p>${currentQuote.quote}</p> 
+      <p class="author>${currentQuote.author}</p>`;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
+}
 
 generateBtn.addEventListener('click', generateRandomQuote);
+toggleFavoriteBtn.addEventListener('click', toggleFavotite);
